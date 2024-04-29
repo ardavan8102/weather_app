466e8d49349dbc5a57923711fa6015628b25b8ab
Ardavan Eskandari <eskandariardavan81@gmail.com>
2024-04-29 18:17:37 +0330

    Constants changed

--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -27,7 +27,7 @@ class myColors {
   // Background Colors
   static const Color light = Color(0xFFF6F6F6);
   static const Color dark = Color(0xFF272727);
-  static const Color primaryBackground = Color(0xFFF3F5FF);
+  static const Color primaryBackground = Color.fromARGB(255, 210, 216, 246);
 
   // Background Container Colors
   static const Color lightContainer = Color(0xFFF6F6F6);
--- lib/utils/constants/text_strings.dart
+++ lib/utils/constants/text_strings.dart
@@ -1,10 +1,10 @@
 class Texts {
   // Get Started Screen
   static const String getStartedTitle1 = "You've Arrived !";
-  static const String getStartedSubTitle1 = "Welcome To Your Weather Application. We Are So happy to Have you Here. You can use below button To Know more about Developer.";
+  static const String getStartedSubTitle1 = "Welcome To Your Weather Application. We Are So happy to Have you Here. Feel Free To Submit any Suggestions and criticisms.";
 
   static const String getStartedTitle2 = "Search For it";
-  static const String getStartedSubTitle2 = "You can Search and See the result in detail and Save all data in Your History cache.";
+  static const String getStartedSubTitle2 = "You can Search and See the result in detail and Save all data in Your History cache.\n\nNOTE : YOUR DATA IS PROTECTED.";
 
   static const String getStartedTitle3 = "Hit The Space";
   static const String getStartedSubTitle3 = "View and check the weather of your city and other cities in the world in the simplest and most elegant way.";
