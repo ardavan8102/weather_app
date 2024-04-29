6dba6596a58b974cb0e824def450f3d831d2792b
Ardavan Eskandari <eskandariardavan81@gmail.com>
2024-04-29 18:27:20 +0330

    Bottom Navigation bar done

--- lib/screens/get_started.dart
+++ lib/screens/get_started.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:url_launcher/url_launcher_string.dart';
 import 'package:weather_app/screens/welcome.dart';
 import 'package:weather_app/utils/constants/image_strings.dart';
 import 'package:weather_app/utils/constants/text_strings.dart';
 import 'package:weather_app/utils/helpers/helper_functions.dart';
-import 'package:weather_app/widgets/animation_with_title.dart';
+import 'package:weather_app/widgets/get%20started/animation_with_title.dart';
 import 'package:weather_app/widgets/custom_page_route.dart';
 
 class GetStarted extends StatelessWidget {
@@ -18,43 +17,37 @@ class GetStarted extends StatelessWidget {
 
     return Scaffold(
       body: SafeArea(
-        child: PageView(
+        child: Stack(
           children: [
-            imageWithTitleAndSubTitle(
-              dark: dark,
-              image: Images.getStartedAnimation1,
-              title: Texts.getStartedTitle1,
-              subTitle: Texts.getStartedSubTitle1,
-              showButton: true,
-              buttonText: 'Ardavan Eskandari',
-              buttonPadding: 12,
-              onPressed: () async {
-                final String url = 'https://github.com/ardavan8102';
-                await launchUrlString(
-                  url,
-                  mode: LaunchMode.inAppBrowserView,
-                );
-              },
-            ),
-
-            imageWithTitleAndSubTitle(
-              dark: dark,
-              image: Images.getStartedAnimation2,
-              title: Texts.getStartedTitle2,
-              subTitle: Texts.getStartedSubTitle2,
-            ),
-
-            imageWithTitleAndSubTitle(
-              dark: dark,
-              image: Images.getStartedAnimation3,
-              title: Texts.getStartedTitle3,
-              subTitle: Texts.getStartedSubTitle3,
-              showButton: true,
-              buttonText: 'Get Started',
-              buttonPadding: 16,
-              onPressed: () {
-                Navigator.push(context, CustomPageRoute(child: WelcomePage()));
-              },
+            PageView(
+              children: [
+                imageWithTitleAndSubTitle(
+                  dark: dark,
+                  image: Images.getStartedAnimation1,
+                  title: Texts.getStartedTitle1,
+                  subTitle: Texts.getStartedSubTitle1,
+                ),
+            
+                imageWithTitleAndSubTitle(
+                  dark: dark,
+                  image: Images.getStartedAnimation2,
+                  title: Texts.getStartedTitle2,
+                  subTitle: Texts.getStartedSubTitle2,
+                ),
+            
+                imageWithTitleAndSubTitle(
+                  dark: dark,
+                  image: Images.getStartedAnimation3,
+                  title: Texts.getStartedTitle3,
+                  subTitle: Texts.getStartedSubTitle3,
+                  showButton: true,
+                  buttonText: 'Get Started',
+                  buttonPadding: 16,
+                  onPressed: () {
+                    Navigator.push(context, CustomPageRoute(child: WelcomePage()));
+                  },
+                ),
+              ],
             ),
           ],
         ),
--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/utils/constants/colors.dart';
+import 'package:weather_app/utils/helpers/helper_functions.dart';
+import 'package:weather_app/widgets/appbar.dart';
+import 'package:weather_app/widgets/bottom_nav_bar.dart';
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({super.key});
@@ -8,10 +12,17 @@ class WelcomePage extends StatefulWidget {
 }
 
 class _WelcomePageState extends State<WelcomePage> {
+
   @override
   Widget build(BuildContext context) {
+    final dark = myHelperFunctions.isDarkMode(context);
+
     return Scaffold(
-      backgroundColor: Colors.red,
+      backgroundColor: dark ? myColors.dark : myColors.primaryBackground,
+      appBar: MainAppBar(),
+      bottomNavigationBar: MainNavBar(),
     );
   }
+
+  
 }
