a4ce6d127998860a09c7221c8c89d05c205ea0ea
ardavan8102 <ardavaneskandari007@gmail.com>
2025-09-07 18:31:02 +0330

    Files Created

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+
+// https://api.openweathermap.org/data/2.5/weather?q=Tehran&appid=1446b9c6b25f5a5f978726164c203ae5&units=imperial
